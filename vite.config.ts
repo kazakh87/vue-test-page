d67a3458219cf2c3025e020ffd931a8de500b1d3
kazakh87 <kazakh87@gmail.com>
2021-07-03 17:37:45 +0200

    publicDir + publicPath added

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default { ...defineConfig({
+  publicDir: "/vue-test-page/",
+  
   plugins: [vue()]
-})
+}),
+publicPath: '/vue-test-page/'
+}
